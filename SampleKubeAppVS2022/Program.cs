28e5ef2a48010d8173eca296116333fadd2ce4af
anongitmous <registrations@ananswerback.com>
2021-11-18 19:36:42 -0700

    if no arguments are supplied, an attempt to build a default config is made.

--- SampleKubeAppVS2022/Program.cs
+++ SampleKubeAppVS2022/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information about this funky new way of writing your 'Main' entry point...
 
-var httpOperationResponse = await GetItems(args[0], args[1]);
+var httpOperationResponse = await GetItems(args.Length > 0 ? args[0] : null, args.Length > 1 ? args[1] : null);
 var body = httpOperationResponse.Body;
 foreach (var item in body.Items)
 {
@@ -16,7 +16,22 @@ foreach (var item in body.Items)
 
 async Task<Microsoft.Rest.HttpOperationResponse<k8s.Models.V1PodList>> GetItems(string kubeCfg, string kubeCtx)
 {
-    using var client = new k8s.Kubernetes(k8s.KubernetesClientConfiguration.BuildConfigFromConfigFile(kubeCfg, kubeCtx));
-    return await client.ListPodForAllNamespacesWithHttpMessagesAsync(null, null, null, null, null, null,
-        null, null, null, null, null).ConfigureAwait(false);
+    k8s.Kubernetes client = null;
+    try
+    {
+        if (null == kubeCfg)
+        {
+            client = new k8s.Kubernetes(k8s.KubernetesClientConfiguration.BuildDefaultConfig());
+        }
+        else
+        {
+            client = new k8s.Kubernetes(k8s.KubernetesClientConfiguration.BuildConfigFromConfigFile(kubeCfg, kubeCtx));
+        }
+        return await client.ListPodForAllNamespacesWithHttpMessagesAsync(null, null, null, null, null, null,
+            null, null, null, null, null).ConfigureAwait(false);
+    }
+    finally
+    {
+        client?.Dispose();
+    }
 }
